9fa3e74b912848d173a71611a4c930937a426ad9
Joseph Terrado <joseph_terrado@hotmail.com>
2023-11-29 22:23:32 -0600

    Function for userinput, header date and name

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,72 @@
 'use strict';
 
 import { onEvent, getElement, select, create } from "./utils.js";
+import { User } from "./user.js";
 
 // Variables
+
+const userInput = select('.input');
+const postButton = select('.post');
+const userOutput = select('.grid-output');
+const insertImage = getElement('#image');
+const imageName = select('.imageName');
+
+
+// functions
+
+function userPost() {
+    const userInputvalue = userInput.value.trim();
+
+    if(userInputvalue !== '') {
+        const postElement = create('div');
+        postElement.classList.add('user-post');
+
+        const userInputDisplay = create('p');
+        userInputDisplay.innerText = userInputvalue;
+
+        postElement.appendChild(userInputDisplay);
+
+        userOutput.appendChild(postElement);
+
+        userInputvalue = '';
+        insertImage.value = '';
+
+        setHeader();
+    }
+}
+
+function imagePreview() {
+    let file = insertImage.files[0];
+
+    if (file) {
+        imageName.innerText = file.name;
+    } else {
+        imageName.innerText = '';
+    }
+}
+
+function setHeader() {
+    const date = getDate();
+    const headerDate = 
+
+}
+
+function getDate() {
+    const currentDate = new Date();
+    const currentMonth = currentDate.toLocaleString('en-US', { month: 'short' });
+    const currentDay = currentDate.getDate();
+    const currentYear = currentYear.getFullYear();
+
+    return `${currentMonth} ${currentDay}, ${currentYear}`;
+}
+
+// Event functions
+
+onEvent('change', insertImage, function() {
+    imagePreview();
+});
+
+onEvent('click', postButton, function() {
+    userPost();
+    setHeader();
+})
--- assets/script/subscriber.js
+++ assets/script/subscriber.js
@@ -1,1 +1,31 @@
 'use strict';
+
+export class Person extends User {
+    #pages;
+    #groups;
+    #canMonetize;
+
+    constructor(
+        id,
+        name,
+        userName,
+        email,
+        pages,
+        groups,
+        canMonetize
+    ){  
+        super(id, name, )
+        this.#pages = pages;
+        this.#groups = groups;
+        this.#canMonetize = canMonetize;
+    }
+
+    get pages() { return this.#pages; }
+    get groups() { return this.#groups; }
+    get canMonetize() { return this.#canMonetize; }
+
+    getInfo() {
+        return `${this.#pages}, ${this.#groups}, ${this.#canMonetize}`;
+    }
+
+}
